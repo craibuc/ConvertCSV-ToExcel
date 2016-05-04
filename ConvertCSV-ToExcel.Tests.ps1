cbad96aa122e22dd39188e2c2302d4922026d0a6
Craig Buchanan <cbuchanan@connecticutchildrens.org>
2016-05-04 12:11:38 -0400

    Adding support for -Path parameter

--- ConvertCSV-ToExcel.Tests.ps1
+++ ConvertCSV-ToExcel.Tests.ps1
@@ -8,9 +8,15 @@ Describe "ConvertCSV-ToExcel" {
     # arrange
     #
 
-    # dummy data
     $nl = [Environment]::NewLine
-    $content = '"DATE_COLUMN","DATETIME_COLUMN","TEXT_COLUMN"' + $nl + '2015-05-01,2015-05-01 23:00:00.000,LOREM IPSUM' + $nl + 'NULL,null,nuLL'
+    $xlsx` = 'Output.xlsx'
+
+    # dummy data (HERE string)
+$Content = @'
+"DATE_COLUMN","DATETIME_COLUMN","TEXT_COLUMN"
+"2015-05-01","2015-05-01 23:00:00.000","LOREM IPSUM"
+"2015-05-01","2015-05-01 23:00:00.000","LOREM IPSUM"
+'@
 
     # create first CSV file and popoulate it with data
     $CSV_0 = New-item "TestDrive:\CSV_0.csv" -Type File
@@ -20,13 +26,27 @@ Describe "ConvertCSV-ToExcel" {
     $CSV_1 = New-item "TestDrive:\CSV_1.csv" -Type File
     Set-Content $CSV_1 -Value $content
 
-    Context "Supplying the manditory parameters; -inputfile via the pipeline" {
+    Context "Supplying the manditory parameters; -InputFile via the pipeline" {
 
-        # act
-        Get-ChildItem 'TestDrive:\*.csv' | ConvertCSV-ToExcel -Output 'Output.xlsx'
+        It -skip "Coonverts more than one CSV-formatted file to a single XLSX-formatted file in the current directory" {
+            # act
+            Get-ChildItem 'TestDrive:\*.csv' | ConvertCSV-ToExcel -Output $xlsx -Verbose
 
-        It "Coonverts more than one CSV-formatted file to a single XLSX-formatted file in the current directory" {
+            # assert
             Get-ChildItem 'Output.xlsx' | Should Exist
+        }
+
+        if ( Test-Path ".\$xlsx" ) { Remove-Item ".\$xlsx"}
+    }
+
+    Context "Supplying the -Path parameter" {
+
+        It "Coonverts more than one CSV-formatted file to a single XLSX-formatted file in the specified directory" {
+            # act
+            Get-ChildItem 'TestDrive:\*.csv' | ConvertCSV-ToExcel -Output $xlsx -Path TestDrive: -Verbose
+
+            # assert
+            Get-ChildItem "TestDrive:\$xlsx" | Should Exist
         }
 
     }
